ef7389687b0af63cf1d358d7192a3cb4363679ed
Kimpan <Kimpan@KIMPANS-PC>
2021-02-05 10:56:45 +0100

    My first commit

---
+++ HC-SR501_Latest/HC-SR501_Latest.ino
@@ -0,0 +1,33 @@
+#define pirSensor 8
+
+int reading;
+
+void setup() 
+{
+
+  pinMode(pirSensor, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+
+
+}
+
+void loop() 
+{
+
+  reading = digitalRead(pirSensor);
+
+  if (reading == HIGH) 
+  {
+    Serial.println("Motion");
+    digitalWrite(13, HIGH);
+    delay (6000);
+  }
+  else 
+  {
+    Serial.println("0");
+    digitalWrite(13, LOW);
+  }
+  delay(3);// Delay for main loop
+
+}
